ec843365609adbb7417180f685e9879840ffc06b
Ahmed Taha <ahmadhamidty@gmail.com>
2023-09-25 13:54:04 +0300

    add packages - auto imports - test

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    Nuxt module playground!
-  </div>
+  <div>Nuxt module playground!</div>
+  <Badge :count="5" />
 </template>
 
-<script setup>
-</script>
+<script setup></script>
--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,47 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import {
+  defineNuxtModule,
+  addPlugin,
+  createResolver,
+  installModule,
+  addComponentsDir,
+} from "@nuxt/kit";
+import { name, version } from "../package.json";
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
-
+console.log(name);
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name,
+    version,
+    configKey: "ikymat-ui",
+    compatibility: {
+      nuxt: "^3.0.0-rc.8",
+    },
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
+
+  // Shorthand sugar to register Nuxt hooks
+  hooks: {},
+
+  async setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url);
+
+    // Transpile runtime
+    const runtimeDir = resolver.resolve("./runtime");
+
+    // Modules
+    await installModule("@unocss/nuxt");
+    await installModule("@vueuse/nuxt");
+
+    // Components
+    addComponentsDir({
+      path: resolver.resolve(runtimeDir, "components"),
+      watch: false,
+    });
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  }
-})
+    addPlugin(resolver.resolve("./runtime/plugin"));
+  },
+});
